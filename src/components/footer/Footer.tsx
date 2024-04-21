1bbf2e043cad801899e135b528908251c02f8cb0
wanda azhar <wandaazhar@gmail.com>
2024-04-21 01:51:18 -0400

    adding footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './footer.scss';
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Built with Love ❤️ By Wanda Azhar in Michigan, USA</p>
+    </footer>
+  );
+}
+
+export default Footer;
